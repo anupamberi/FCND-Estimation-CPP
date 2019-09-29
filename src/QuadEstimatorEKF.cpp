a9a81187380f07aaa6e9afa2fe38ecb344858239
Anupam BERI <anupamberi@gmail.com>
2019-09-29 10:42:16 +0200

    Scenario 2 works.

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,9 +93,14 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  // Create a Quaternion from the current Roll, Pitch and Yaw
+  Quaternion<float> qt = SLR::Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+  // Creat a Quaternion dq from the measurement of the angular rates from IMU in the body frame
+  Quaternion<float> qtBar = qt.IntegrateBodyRate(gyro, dtIMU);
+
+  float predictedPitch = qtBar.Pitch();
+  float predictedRoll = qtBar.Roll();
+  ekfState(6) = qtBar.Yaw();
 
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
